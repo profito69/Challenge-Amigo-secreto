b43fb2d80f26851b602553720c19dce41758f4db
profito60 <20200118@lamolina.edu.pe>
2025-03-11 22:26:05 -0500

    Mejorado

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let nombres = [];
 
 function agregarAmigo(){
+    document.getElementById("resultado").textContent ="";
     let textoingresado = document.getElementById("amigo").value;
 
     if (textoingresado.trim() !== ""){
@@ -18,10 +19,9 @@ function sortearAmigo(){
         alert("No hay nombres en la lista para sortear.");
         return;
     }
-    let nombreAleatorio = nombres[Math.floor(Math.random() * nombres.length)]; // Selección aleatoria
+    let nombreAleatorio = nombres[Math.floor(Math.random() * nombres.length)]; 
             document.getElementById("resultado").textContent = "Nombre sorteado: " + nombreAleatorio;
 
-            // Limpiar la lista visualmente y el array
             document.getElementById("listaAmigos").innerHTML = "";
             nombres = [];
 }
