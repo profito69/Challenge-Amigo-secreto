955d045caca3484fd7932ce9e7e91b973805b522
profito60 <20200118@lamolina.edu.pe>
2025-03-11 22:07:32 -0500

    Agregación en lista

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let lista = [];
+
+function agregarAmigo(){
+    let textoingresado = document.getElementById("amigo").value;
+
+    if (textoingresado.trim() !== ""){
+        let lista = document.getElementById("listaAmigos");
+        let nuevoElemento = document.createElement("li");
+        nuevoElemento.textContent = textoingresado;
+        
+        lista.appendChild(nuevoElemento);
+    }
+    document.getElementById("amigo").value = "";
+}
