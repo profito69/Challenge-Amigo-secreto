d0ed31fb88faafec3cea13f7895c4c196105ec67
profito60 <20200118@lamolina.edu.pe>
2025-03-11 22:16:15 -0500

    agregamos sorteo

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let lista = [];
+let nombres = [];
 
 function agregarAmigo(){
     let textoingresado = document.getElementById("amigo").value;
 
     if (textoingresado.trim() !== ""){
+        nombres.push(textoingresado);
         let lista = document.getElementById("listaAmigos");
         let nuevoElemento = document.createElement("li");
         nuevoElemento.textContent = textoingresado;
@@ -12,4 +12,16 @@ function agregarAmigo(){
         lista.appendChild(nuevoElemento);
     }
     document.getElementById("amigo").value = "";
+}
+function sortearAmigo(){
+    if (nombres.length === 0) {
+        alert("No hay nombres en la lista para sortear.");
+        return;
+    }
+    let nombreAleatorio = nombres[Math.floor(Math.random() * nombres.length)]; // Selección aleatoria
+            document.getElementById("resultado").textContent = "Nombre sorteado: " + nombreAleatorio;
+
+            // Limpiar la lista visualmente y el array
+            document.getElementById("listaAmigos").innerHTML = "";
+            nombres = [];
 }
